34d42e170557867d841fcfafaa21c97e8baf936c
mohamedishack <hishackdeen@gmail.com>
2024-03-22 17:59:37 +0530

    we will use an input's value to search for users in GitHub using their username or email.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const API_URL = "https://api.github.com";
+// const response = await fetch(`${API_URL}/search/users?q=${query}`);
+const Form = ({ onSubmit, onChange, value }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={onSubmit} onChange={onChange} className="search-form">
+      <input
+        id="search"
+        type="text"
+        value={value}
+        placeholder="Enter your username or email"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+const User = ({ avatar, url, username }) => {
+  return (
+    <div className="user">
+      <img src={avatar} alt="Profile" width="50" height="50" />
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {username}
+      </a>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const onSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await fetch(`${API_URL}/search/users?q=${query}`);
+      const json = await response.json();
+      setResults(json.items);
+    } catch (e) {
+      throw new Error(e);
+    }
+  };
+  return (
+    <div className="App">
+      <main>
+        <h1>Project 5: Github User search</h1>
+        <Form onSubmit={onSubmit} onChange={onChange} value={query} />
+        <h3>Results</h3>
+        <div id="results">
+          {results.map((user) => (
+            <User
+              key={user.login}
+              avatar={user.avatar_url}
+              url={user.html_url}
+              username={user.login}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
